abfc21163d4eff8ced7110765c0e5afe9175ad20
曾宇平 <zengyuping@fangyibao.cn>
2021-06-03 16:12:59 +0800

    修复istio下图片服务访问图片404的问题，增加sentinel相关yaml配置

---
+++ Services/PublicService/Infrastructure/Common/AliSentinelConfigBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Common
+{
+    /// <summary>
+    /// 针对阿里sentinel组件的dapr支持
+    /// </summary>
+    public class AliSentinelConfigBuilder
+    {
+
+    }
+}
